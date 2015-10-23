ded7b30241d78eb0839571ed3270b07c71fab196
Carsten Griesheimer <hallo@carstengriesheimer.de>
2015-10-23 15:59:56 +0200

    Responsive design

--- pages/index.jsx
+++ pages/index.jsx
@@ -34,13 +34,15 @@ IndexPage = ReactMeteor.createClass({
         return (<li key={idea._id}><IdeaPost idea={idea} /></li>);
       });
     } else {
-      var items = [<li key="_">Keine Einträge</li>]
+      var items = [<li key="_" className="padded">Keine Einträge</li>]
     }
     return (
       <div className="index-page">
-        <h1>Vorschläge für ein besseres <span style={{color: "#009E82"}}>KIT</span></h1>
-        <IdeaForm authenticated={this.state.authenticated}/>
-        <FilterBar filter={this.state.filter}/>
+        <div className="padded">
+          <h1>Vorschläge für ein besseres <span style={{color: "#009E82"}}>KIT</span></h1>
+          <IdeaForm authenticated={this.state.authenticated}/>
+          <FilterBar filter={this.state.filter}/>
+        </div>
         <ul className="suggestion-list">{items}</ul>
       </div>
     );
